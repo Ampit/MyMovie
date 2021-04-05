208d0c7c8c5ad05ec786714a78596341f84d9859
Amarpreet Singh <ampit.xd@gmail.com>
2021-04-05 18:12:45 -0400

    Deployment updates

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
@@ -24,6 +25,14 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 authRoutes(app);
+
+if (process.env.NODE_ENV == "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
