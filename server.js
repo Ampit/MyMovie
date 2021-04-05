584ec0676df22c883637ae9bb293d2314d708a4a
Amarpreet Singh <ampit.xd@gmail.com>
2021-04-05 19:18:45 -0400

    React Deploy Fix

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 authRoutes(app);
 
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 80;
